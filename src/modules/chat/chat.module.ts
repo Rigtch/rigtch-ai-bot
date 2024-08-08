f838c068692046ae025d60bbdbf24c7a4e130ebb
Mnigos <makowskiigor@gmail.com>
2024-08-08 14:41:32 +0200

    feat(modules/chat): create `ChatVectorStore` provider

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 
 import { ChatModel } from './chat.model'
 import { ChatService } from './chat.service'
+import { ChatVectorStore } from './chat.vector-store'
 
 @Module({
-  providers: [ChatModel, ChatService],
+  providers: [ChatModel, ChatService, ChatVectorStore],
   exports: [ChatService],
 })
 export class ChatModule {}
