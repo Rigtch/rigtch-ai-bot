b03bfc4faaefe4c463f68e9ea7a4ead256917735
Mnigos <makowskiigor@gmail.com>
2024-08-08 21:10:53 +0200

    docs(main): setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 import { EnvService } from '@config/env'
 import { AppModule } from '@modules/app'
@@ -8,6 +9,20 @@ import { AppModule } from '@modules/app'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const envService = app.get(EnvService)
+
+  const documentConfig = new DocumentBuilder()
+    .setTitle('Henry Chat API')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, documentConfig)
+
+  SwaggerModule.setup('api', app, document)
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  })
 
   await app.listen(envService.get('PORT'))
 }
